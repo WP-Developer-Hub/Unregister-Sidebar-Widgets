2c6509c8e62cc1001dc9814035556a4d1544c318
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2025-01-07 10:23:14 -0500

    Refactor and update unregister sidebar widgets plugin
    - Renamed text domain from 'github_profile_widget' to 'unregister_sidebar_widget' for better consistency.
    - Organized the class and method structure, ensuring improved readability and functionality.
    - Updated function and variable names to follow consistent naming conventions.
    - Improved admin interface language to reflect updated text domain.
    - Refactored logic to ensure widgets are properly unregistered and handled, including SiteOrigin panels support.
    - Added better documentation for functions and added action hooks for widget registration/unregistration.

--- unregister-widgets.php
+++ unregister-widgets.php
@@ -4,13 +4,13 @@ Plugin Name: Unregister Sidebar Widgets
 Plugin URI: https://github.com/ShinichiNishikawa/Unregister-Sidebar-Widgets
 Description: You can choose and unregister/disable/hide widgets which you don't need, both defaults and added by plugins.
 Author: Shinichi Nishikawa & DJABhipHop
-Requires PHP: 7.2 
-Requires at least: 6.0 
-License: GPL2 or later 
+Requires PHP: 7.2
+Requires at least: 6.0
+License: GPL2 or later
 Version: 2.0
 Author URI: http://nskw-style.com
-Text Domain: github_profile_widget 
-Domain Path: /languages 
+Text Domain: unregister_sidebar_widget
+Domain Path: /languages
 
 License:
  Released under the GPL license
@@ -37,215 +37,208 @@ License:
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
+if ( ! class_exists( 'unregister_sidebar_widgets' ) ) {
+    class unregister_sidebar_widgets {
 
-class unregister_sidebar_widgets {
+        public $classes = array();
+        public $classes_re = array();
+        public $classes_un = array();
 
-    public $classes = array();
-    public $classes_re = array();
-    public $classes_un = array();
-
-    function __construct() {
-        add_action( 'admin_menu',   array( $this, 'menu' )       );
-        add_action( 'admin_init',   array( $this, 'save' )       );
-        add_action( 'widgets_init', array( $this, 'unregister' ), 15 );
-        add_action( 'siteorigin_panels_widgets', array( $this, 'unregister_so' ) );
-        
-    }
-
-    // add menu
-    public function menu() {
-        add_theme_page(
-            __( 'Unregister Widgets', 'widget-name' ),
-            __( 'Unregister Widgets', 'widget-name' ),
-            'activate_plugins',
-            'unregister_widget',
-            array( $this, 'form' )
-        );
-    }
-
-    // get array of UNregistered widgets
-    // from the DB.
-    public function get_unregistered() {
-        
-        $from_db = get_option( 'unregid_classes' );
-        if ( $from_db ) {
-            $this->classes_un = $from_db;
-        } else {
-            $this->classes_un = array();
+        function __construct() {
+            add_action( 'admin_menu',   array( $this, 'menu' )       );
+            add_action( 'admin_init',   array( $this, 'save' )       );
+            add_action( 'widgets_init', array( $this, 'unregister' ), 15 );
+            add_action( 'siteorigin_panels_widgets', array( $this, 'unregister_so' ) );
+            
         }
-        
-        
-    }
 
-    // make associative array of registered widgets
-    // from $wp_registered_widgets global variable.
-    public function get_registered() {
-        
-        global $wp_registered_widgets;
-        
-        foreach ( $wp_registered_widgets as $rw ) {
-
-            $obj   = $rw['callback'][0];
-            $class = get_class( $obj );
-
-            $this->classes_re[$class] = $this->class_to_namedesc( $class );
-
+        // add menu
+        public function menu() {
+            add_theme_page(
+                __( 'Unregister Widgets', 'unregister_sidebar_widget' ),
+                __( 'Unregister Widgets', 'unregister_sidebar_widget' ),
+                'activate_plugins',
+                'unregister_widget',
+                array( $this, 'form' )
+            );
         }
-        
-    }
 
-    // return name & desc array by given class name.
-    // it's possible only for registered widgets.
-    public function class_to_namedesc( $class ) {
-        global $wp_widget_factory;
+        // get array of UNregistered widgets
+        // from the DB.
+        public function get_unregistered() {
+            $from_db = get_option( 'unregid_classes' );
+            if ( $from_db ) {
+                $this->classes_un = $from_db;
+            } else {
+                $this->classes_un = array();
+            }
+        }
 
-        // Check if the class exists in the widget factory.
-        if ( isset( $wp_widget_factory->widgets[ $class ] ) ) {
-            $obj = $wp_widget_factory->widgets[ $class ];
+        // make associative array of registered widgets
+        // from $wp_registered_widgets global variable.
+        public function get_registered() {
+            global $wp_registered_widgets;
 
-            // Get name and description, with fallbacks.
-            $name = $obj->widget_options['name'] ?? $class; // Replace spaces with underscores in the class name.
-            $desc = $obj->widget_options['description'] ?? 'No description available.';
+            foreach ( $wp_registered_widgets as $rw ) {
+                $obj   = $rw['callback'][0];
+                $class = get_class( $obj );
 
+                $this->classes_re[$class] = $this->class_to_namedesc( $class );
+            }
+        }
+
+        // return name & desc array by given class name.
+        // it's possible only for registered widgets.
+        public function class_to_namedesc( $class ) {
+            global $wp_widget_factory;
+
+            // Check if the class exists in the widget factory.
+            if ( isset( $wp_widget_factory->widgets[ $class ] ) ) {
+                $obj = $wp_widget_factory->widgets[ $class ];
+
+                // Get name and description, with fallbacks.
+                $name = $obj->widget_options['name'] ?? $class; // Replace spaces with underscores in the class name.
+                $desc = $obj->widget_options['description'] ?? 'No description available.';
+
+                return [
+                    'name' => $name,
+                    'desc' => $desc,
+                ];
+            }
+
+            // Return default values if class is not found.
             return [
-                'name' => $name,
-                'desc' => $desc,
+                'name' => $class,
+                'desc' => 'No description available.',
             ];
         }
 
-        // Return default values if class is not found.
-        return [
-            'name' => $class,
-            'desc' => 'No description available.',
-        ];
-    }
+        // save the key[class]=>[name=>name, desc=>desc] array
+        public function save() {
+            if ( isset( $_POST['uw-submit'] ) && $_POST['uw-submit'] && check_admin_referer( 'uw-display-form', 'unregister_widget' ) ) {
 
-    // save the key[class]=>[name=>name, desc=>desc] array
-    public function save() {
-
-        if ( isset( $_POST['uw-submit'] ) && $_POST['uw-submit'] && check_admin_referer( 'uw-display-form', 'unregister_widget' ) ) {
-
-            $dont_save = array( 'unregister_widget', '_wp_http_referer', 'uw-submit' );
-            
-            foreach ( $dont_save as $dn ) {
-                if ( isset( $posted[$dn] ) ) {
-                    unset($posted[$dn]);
+                $dont_save = array( 'unregister_widget', '_wp_http_referer', 'uw-submit' );
+                
+                foreach ( $dont_save as $dn ) {
+                    if ( isset( $posted[$dn] ) ) {
+                        unset($posted[$dn]);
+                    }
                 }
-            }
-            
-            if ( isset( $_POST['uw_widgets'] ) ) {
-                $unregid_classes = array();
-                foreach ( $_POST['uw_widgets'] as $class_name ) {
-                    $unregid_classes[ $class_name ] = $this->class_to_namedesc( $class_name );
+                
+                if ( isset( $_POST['uw_widgets'] ) ) {
+                    $unregid_classes = array();
+                    foreach ( $_POST['uw_widgets'] as $class_name ) {
+                        $unregid_classes[ $class_name ] = $this->class_to_namedesc( $class_name );
+                    }
                 }
-            }
+                
+                $updated = update_option( 'unregid_classes', $unregid_classes );
             
-            $updated = update_option( 'unregid_classes', $unregid_classes );
-        
-            if ( $updated ) {
-                add_action( 'admin_notices', array( $this, 'notice' ) );
+                if ( $updated ) {
+                    add_action( 'admin_notices', array( $this, 'notice' ) );
+                }
+            
             }
-        
+
         }
 
-    }
-
-    // admin notice
-    public function notice() {
-        ?>
-        <div class="updated">
-            <ul>
-                <li><?php echo esc_html__( 'Saved! The widgets you chose have been hidden. :)', 'widget-name' ); ?> <a href="<?php echo admin_url( 'widgets.php' ); ?>"><?php esc_html_e( 'Widgets Page', 'widget-name' ); ?></a></li>
-            </ul>
-        </div>
-        <?php
-    }
-
-    // unregister actually
-    public function unregister() {
-        $this->get_unregistered();
-        $unregid = array_keys( $this->classes_un );
-        foreach ( $unregid as $un ) {
-            unregister_widget($un);
-            error_log($un);
+        // admin notice
+        public function notice() {
+            ?>
+            <div class="updated">
+                <ul>
+                    <li><?php echo esc_html__( 'Saved! The widgets you chose have been hidden. :)', 'unregister_sidebar_widget' ); ?> <a href="<?php echo admin_url( 'widgets.php' ); ?>"><?php esc_html_e( 'Widgets Page', 'unregister_sidebar_widget' ); ?></a></li>
+                </ul>
+            </div>
+            <?php
         }
-    }
 
-    public function unregister_so($widgets) {
-        $this->get_unregistered();
-        $unregid = array_keys( $this->classes_un );
-        foreach ( $unregid as $un ) {
-            unset($widgets[$un]);
+        // unregister actually
+        public function unregister() {
+            $this->get_unregistered();
+            $unregid = array_keys( $this->classes_un );
+            foreach ( $unregid as $un ) {
+                unregister_widget($un);
+                error_log($un);
+            }
         }
-    }
 
-    // display the form
-    public function form() {
-        ?>
-        <div class="wrap">
-            <h1><?php esc_html_e( 'Unregister Widgets', 'widget-name' ); ?></h1>
-            <form id="display-form" method="post" action="">
-                <h2><?php esc_html_e( 'Choose widgets to unregister', 'widget-name' ); ?></h2>
-                <div class="tablenav top">
-                    <div class="alignleft actions bulkactions">
-                        <?php submit_button( __( 'Save Widgets', 'widget-name' ), 'button action', 'uw-submit' ); ?>
+        public function unregister_so($widgets) {
+            $this->get_unregistered();
+            $unregid = array_keys( $this->classes_un );
+            foreach ( $unregid as $un ) {
+                unset($widgets[$un]);
+            }
+        }
+
+        // display the form
+        public function form() {
+            ?>
+            <div class="wrap">
+                <h1><?php esc_html_e( 'Unregister Widgets', 'unregister_sidebar_widget' ); ?></h1>
+                <form id="display-form" method="post" action="">
+                    <h2><?php esc_html_e( 'Choose widgets to unregister', 'unregister_sidebar_widget' ); ?></h2>
+                    <div class="tablenav top">
+                        <div class="alignleft actions bulkactions">
+                            <?php submit_button( __( 'Save Widgets', 'unregister_sidebar_widget' ), 'button action', 'uw-submit' ); ?>
+                        </div>
+                        <br class="clear">
                     </div>
-                    <br class="clear">
-                </div>
-                <table class="wp-list-table widefat plugins">
-                    <thead>
-                        <tr>
-                            <td id="cb" class="manage-column column-cb check-column">
-                                <input id="cb-select-all-1" type="checkbox">
-                            </td>
-                            <th scope="col"><?php esc_html_e( 'Widget Name', 'widget-name' ); ?></th>
-                            <th scope="col"><?php esc_html_e( 'Description', 'widget-name' ); ?></th>
-                            <th scope="col"><?php esc_html_e( 'Status', 'widget-name' ); ?></th>
-                        </tr>
-                    </thead>
-                    <tbody id="the-list">
-                        <?php
-                        $this->get_registered();
-                        $this->get_unregistered();
-
-                        $all_wids = array_merge( $this->classes_re, $this->classes_un );
-                        $already_unregistered = array_keys( $this->classes_un );
-                        foreach ( $all_wids as $key => $val ) {
-                            ?>
-                            <tr <?php echo in_array( $key, $already_unregistered ) ? 'class="active"' : 'class="inactive"'; ?> data-slug="<?php echo esc_attr( $key ); ?>" data-plugin="<?php echo esc_attr( $key ); ?>" >
-                                <th scope="row" class="check-column">
-                                    <input
-                                        type="checkbox"
-                                        name="uw_widgets[]"
-                                        id="<?php echo esc_attr( $key ); ?>"
-                                        value="<?php echo esc_attr( $key ); ?>"
-                                        <?php checked( in_array( $key, $already_unregistered ) ); ?>
-                                    />
-                                </th>
-                                <td>
-                                    <label for="<?php echo esc_attr( $key ); ?>">
-                                        <?php echo esc_html( $val['name'] ); ?>
-                                    </label>
+                    <table class="wp-list-table widefat plugins">
+                        <thead>
+                            <tr>
+                                <td id="cb" class="manage-column column-cb check-column">
+                                    <input id="cb-select-all-1" type="checkbox">
                                 </td>
-                                <td><?php echo esc_html( $val['desc'] ); ?></td>
-                            <td><?php echo esc_html( is_active_widget( false, false, $key, false ) ? __( 'Active', 'widget-name' ) : __( 'Inactive', 'widget-name' ) ); ?></td>                            </tr>
+                                <th scope="col"><?php esc_html_e( 'Widget Name', 'unregister_sidebar_widget' ); ?></th>
+                                <th scope="col"><?php esc_html_e( 'Description', 'unregister_sidebar_widget' ); ?></th>
+                            </tr>
+                        </thead>
+                        <tbody id="the-list">
                             <?php
-                            $num++;
-                        } // end foreach.
-                        ?>
-                    </tbody>
-                </table>
-                <?php wp_nonce_field( 'uw-display-form', 'unregister_widget' ); ?>
-                <div class="tablenav bottom">
-                    <div class="alignleft actions bulkactions">
-                        <?php submit_button( __( 'Save Widgets', 'widget-name' ), 'button action', 'uw-submit' ); ?>
-                    </div>
-                    <br class="clear">
-                </div>
-        </form>
-        </div>
-        <?php
-    }
-}
+                            $this->get_registered();
+                            $this->get_unregistered();
 
-new unregister_sidebar_widgets();
+                            $all_wids = array_merge( $this->classes_re, $this->classes_un );
+                            $already_unregistered = array_keys( $this->classes_un );
+                            foreach ( $all_wids as $key => $val ) {
+                                ?>
+                                <tr <?php echo in_array( $key, $already_unregistered ) ? 'class="active"' : 'class="inactive"'; ?> data-slug="<?php echo esc_attr( $key ); ?>" data-plugin="<?php echo esc_attr( $key ); ?>" >
+                                    <th scope="row" class="check-column">
+                                        <input
+                                            type="checkbox"
+                                            name="uw_widgets[]"
+                                            id="<?php echo esc_attr( $key ); ?>"
+                                            value="<?php echo esc_attr( $key ); ?>"
+                                            <?php checked( in_array( $key, $already_unregistered ) ); ?>
+                                        />
+                                    </th>
+                                    <td>
+                                        <label for="<?php echo esc_attr( $key ); ?>">
+                                            <?php echo esc_html( $val['name'] ); ?>
+                                        </label>
+                                        <div class="row-actions">
+                                            <span class="edit"><a href="javascript:void(0)" aria-label="<?php echo esc_attr( $val['name'] ); ?>”">Edit</a></span>
+                                        </div>
+                                    </td>
+                                    <td><?php echo esc_html( $val['desc'] ); ?></td>
+                                <?php
+                                $num++;
+                            } // end foreach.
+                            ?>
+                        </tbody>
+                    </table>
+                    <?php wp_nonce_field( 'uw-display-form', 'unregister_widget' ); ?>
+                    <div class="tablenav bottom">
+                        <div class="alignleft actions bulkactions">
+                            <?php submit_button( __( 'Save Widgets', 'unregister_sidebar_widget' ), 'button action', 'uw-submit' ); ?>
+                        </div>
+                        <br class="clear">
+                    </div>
+            </form>
+            </div>
+            <?php
+        }
+    }
+    new unregister_sidebar_widgets();
+}
